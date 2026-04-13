e6ff5e5a61bd77532d88e7e425091481523a1694
FSoteloLabat <fsotelolabat@frba.utn.edu.ar>
2026-04-13 23:41:41 +0000

    Swap conexión SIN PUERTO

--- swap/src/main.c
+++ swap/src/main.c
@@ -1,9 +1,11 @@
-#include "utils/hello.h"
+#include <commons/config.h>
+#include <commons/log.h>
 #include <stdio.h>
-#include <config.c>
-#include <log.c>
+#include <stdlib.h>
 #include <sys/socket.h>
-#include <utils-server.c>
+#include <utils-server.h>
+
+#include "utils/hello.h"
 
 int main(int argc, char* argv[])
 {
@@ -11,24 +13,37 @@ int main(int argc, char* argv[])
   int socket_swap;
   char* puerto;
   char* ip;
-  char * log_levelstr;
+  char* log_levelstr;
 
-  t_config *config_create("config.conf");
+  t_config* config_create("config.conf");
 
-  char* log_levelstr = config_get_string_value(t_config, "LOG_LEVEL"); 
+  char* log_levelstr = config_get_string_value(t_config, "LOG_LEVEL");
 
   t_log_level log_level = log_level_from_string(log_levelstr);
 
-  t_log* logger = log_create("swap.log", "SWAP", true, log_level); //inicio el log para poder enviar los logs
+  t_log* logger =
+      log_create("swap.log", "SWAP", true,
+                 log_level);  // inicio el log para poder enviar los logs
 
-  ip = config_get_string_value(t_config,"IP"); // Obtengo la IP del archivo de configs
+  ip = config_get_string_value(t_config,
+                               "IP");  // Obtengo la IP del archivo de configs
 
-  puerto = config_get_string_value(t_congig, "PUERTO"); //Obtengo el puerto 
-  
-  socket_swap = crear_conexion(ip,puerto); //Establezco conexión
+  puerto = config_get_string_value(t_congig, "PUERTO");  // Obtengo el puerto
 
-  log_info("swap.log", "## Conectado a Kernel Memory"); //Loggeo el comentario de conexion iniciada
-  
+  socket_swap = crear_conexion(ip, puerto);  // Establezco conexión
+
+  if (socket_swap == -1)
+  {
+    log_info("swap.log", "#ERROR DE CONEXION");  // Informo error
+
+    exit(EXIT_FAILURE);  // Termino el programa
+  }
+  else
+  {
+    log_info("swap.log",
+             "## Conectado a Kernel Memory");  // Loggeo el comentario de
+                                               // conexion iniciada
+  };
   saludar("swap");
   return 0;
 }
