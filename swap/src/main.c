fa2d12937773280874d6e7804e8e89c584d57d2e
FSoteloLabat <fsotelolabat@frba.utn.edu.ar>
2026-04-13 23:25:31 +0000

    Primer commit swap

--- swap/src/main.c
+++ swap/src/main.c
@@ -1,9 +1,34 @@
 #include "utils/hello.h"
 #include <stdio.h>
-#include <commons.io>
+#include <config.c>
+#include <log.c>
+#include <sys/socket.h>
+#include <utils-server.c>
 
 int main(int argc, char* argv[])
 {
+  t_config config;
+  int socket_swap;
+  char* puerto;
+  char* ip;
+  char * log_levelstr;
+
+  t_config *config_create("config.conf");
+
+  char* log_levelstr = config_get_string_value(t_config, "LOG_LEVEL"); 
+
+  t_log_level log_level = log_level_from_string(log_levelstr);
+
+  t_log* logger = log_create("swap.log", "SWAP", true, log_level); //inicio el log para poder enviar los logs
+
+  ip = config_get_string_value(t_config,"IP"); // Obtengo la IP del archivo de configs
+
+  puerto = config_get_string_value(t_congig, "PUERTO"); //Obtengo el puerto 
+  
+  socket_swap = crear_conexion(ip,puerto); //Establezco conexión
+
+  log_info("swap.log", "## Conectado a Kernel Memory"); //Loggeo el comentario de conexion iniciada
+  
   saludar("swap");
   return 0;
 }
