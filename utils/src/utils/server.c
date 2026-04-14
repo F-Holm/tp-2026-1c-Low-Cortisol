c965158e7b1e5941d53dc556b620f2ea63ce1bd1
F-Holm <moshishafcdodgecharger@gmail.com>
2026-04-14 02:40:38 +0000

    fix utils/server/recibir_mensaje(), no retornaba el mensaje

--- utils/src/utils/server.c
+++ utils/src/utils/server.c
@@ -64,12 +64,11 @@ void* recibir_buffer(int* size, int socket_cliente)
   return buffer;
 }
 
-void recibir_mensaje(int socket_cliente)
+// acordarse de liberar memoria dinamica del puntero retornado
+char* recibir_mensaje(int socket_cliente)
 {
   int size;
   char* buffer = recibir_buffer(&size, socket_cliente);
-  log_info(logger, "Me llego el mensaje %s", buffer);
-  free(buffer);
 }
 
 t_list* recibir_paquete(int socket_cliente)
