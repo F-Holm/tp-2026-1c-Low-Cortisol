662b1549b19c5a08a566b6a4c174dd5c66484189
F-Holm <moshishafcdodgecharger@gmail.com>
2026-04-14 02:51:20 +0000

    utils: fix return recibir_mensaje

--- utils/src/utils/server.c
+++ utils/src/utils/server.c
@@ -68,7 +68,7 @@ void* recibir_buffer(int* size, int socket_cliente)
 char* recibir_mensaje(int socket_cliente)
 {
   int size;
-  char* buffer = recibir_buffer(&size, socket_cliente);
+  return recibir_buffer(&size, socket_cliente);
 }
 
 t_list* recibir_paquete(int socket_cliente)
