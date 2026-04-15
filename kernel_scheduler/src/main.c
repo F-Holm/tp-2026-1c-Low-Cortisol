afb636045fb8e677d9c24362a8e3a7c97666b0f1
incutti <mateincu@gmail.com>
2026-04-14 21:33:23 -0300

    kernel scheduler: conexion cliente terminada (no tested)

--- kernel_scheduler/src/main.c
+++ kernel_scheduler/src/main.c
@@ -14,25 +14,33 @@ int main(int argc, char* argv[])
 	char* puerto;
   char* valor;
 
+
 	t_log* logger;
 	t_config* config;
 
   //borrar esto despues de probar
   saludar("kernel_scheduler");
   
-	logger = iniciar_logger();
   config = iniciar_config();
-
+	logger = iniciar_logger(config);
+  
   ip = config_get_string_value(config, "IP");
   puerto = config_get_string_value(config, "PUERTO");
   valor = "Hola, soy el kernel scheduler";
 
-  //conectar con kernel memory como cliente
+
+  //conectar con kernel memory como cliente y loggear el resultado
   conexion = crear_conexion(ip, puerto);
+  if (conexion == -1) {
+    log_error(logger, "Fallo la conexion con kernel memory en %s:%s", ip, puerto);
+    terminar_programa(conexion, logger, config);
+    return 1;
+  } else {
+    log_info(logger, "Conexion establecida con kernel memory en %s:%s", ip, puerto);
+  }
   enviar_mensaje(valor, conexion);
   paquete(conexion, valor);
-  //falta hacer el log de conexion
-
+  //evaluar meter en una funcion lineas 33 a 42
 
   //iniciar servidor para cpu y io
 
@@ -40,16 +48,19 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-t_log* iniciar_logger(void)
+t_log* iniciar_logger(t_config* config)
 {
 	t_log* nuevo_logger;
-	nuevo_logger = log_create("tp0.log", "logsTP0", true, LOG_LEVEL_INFO);
+  char* log_levelstr = config_get_string_value(config, "LOG_LEVEL");
+  t_log_level log_level = log_level_from_string(log_levelstr);
+	nuevo_logger = log_create("kernel_scheduler.log", "kernelScheduler", true, log_level);
 	return nuevo_logger;
 }
+
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-	nuevo_config = config_create("cliente.config");
+	nuevo_config = config_create("kernel_scheduler.config");
 	return nuevo_config;
 }
 
--- kernel_scheduler/src/main.h
+++ kernel_scheduler/src/main.h
@@ -1,7 +1,7 @@
 #include <commons/config.h>
 #include "commons/log.h"
 
-t_log* iniciar_logger(void);
+t_log* iniciar_logger(t_config* config);
 t_config* iniciar_config(void);
 void paquete(int conexion, char* valor);
 void terminar_programa(int conexion, t_log* logger, t_config* config);
