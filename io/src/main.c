7d1ab12b0159388501d1724dd61aab5fa8c88817
FSoteloLabat <fsotelolabat@frba.utn.edu.ar>
2026-04-15 22:30:31 +0000

    x

--- io/src/main.c
+++ io/src/main.c
@@ -4,20 +4,21 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
-#include "utils-client.h"
+#include "utils/client.h"
 #include "utils/hello.h"
+#include "utils/msg.h"
 
 int main()
 {
-  t_config config;
+  t_config* config;
   int socket_IO;
   char* puerto;
   char* ip;
   char* log_levelstr;
 
-  t_config* config_create("IOconf.conf");
+  config = config_create("IOconf.conf");
 
-  char* log_levelstr = config_get_string_value(t_config, "LOG_LEVEL");
+  log_levelstr = config_get_string_value(config, "LOG_LEVEL");
 
   t_log_level log_level = log_level_from_string(log_levelstr);
 
@@ -25,24 +26,49 @@ int main()
       log_create("IO.log", "IO", true,
                  log_level);  // inicio el log para poder enviar los logs
 
-  ip = config_get_string_value(t_config,
+  ip = config_get_string_value(config,
                                "IP");  // Obtengo la IP del archivo de configs
 
-  puerto = config_get_string_value(t_config, "PORT");  // Obtengo el puerto
+  puerto = config_get_string_value(config, "PORT");  // Obtengo el puerto
 
   socket_IO = crear_conexion(ip, puerto);  // Establezco conexión
 
   if (socket_IO == -1)
   {
-    log_info("IO.log", "#ERROR DE CONEXION");  // Informo error
-
-    exit(EXIT_FAILURE);  // Termino el programa
+    log_error(logger, "#ERROR DE CONEXION");  // Informo error
+    log_destroy (logger);
+    config_destroy (config);
+    return EXIT_FAILURE;  // Termino el programa
   }
   else
   {
-    log_info("IO.log",
+    log_info(logger,
              "## Conectado a Kernel Scheduler");  // Loggeo el comentario de
                                                   // conexion iniciada
   };
+  
+  //Handshake con kernel scheduler 
+  enviar_handshake (MID_IO,socket_IO);
+  int id_modulo = recibir_handshake(socket_IO);
+  if(id_modulo != MID_IO){
+    log_error(logger, "## Error en el Handshake con Kernel Scheduler");
+    close(socket_IO);
+    log_destroy(logger);
+    config_destroy(config);
+    return EXIT_FAILURE;
+  }
+  log_info(logger, "## Handshake exitoso con Kernel Scheduler");
+
+  
+  
+  
+  
+  
+  
   return 0;
+
+
+
+
+
 }
