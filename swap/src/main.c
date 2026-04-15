6a88bcf722ef6ddf99491b148d140e784110e14a
FSoteloLabat <fsotelolabat@frba.utn.edu.ar>
2026-04-15 19:31:41 +0000

    IO Y SWAP FINAL S/PUERTO

--- swap/src/main.c
+++ swap/src/main.c
@@ -4,20 +4,21 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
-#include "utils-client.h"
+#include "utils/client.h"
 #include "utils/hello.h"
+#include "utils/msg.h"
 
 int main(int argc, char* argv[])
 {
-  t_config config;
+  t_config* config;
   int socket_swap;
   char* puerto;
   char* ip;
   char* log_levelstr;
 
-  t_config* config_create("config.conf");
+  config = config_create("config.conf");
 
-  char* log_levelstr = config_get_string_value(t_config, "LOG_LEVEL");
+  log_levelstr = config_get_string_value(config, "LOG_LEVEL");
 
   t_log_level log_level = log_level_from_string(log_levelstr);
 
@@ -25,25 +26,36 @@ int main(int argc, char* argv[])
       log_create("swap.log", "SWAP", true,
                  log_level);  // inicio el log para poder enviar los logs
 
-  ip = config_get_string_value(t_config,
+  ip = config_get_string_value(config,
                                "IP");  // Obtengo la IP del archivo de configs
 
-  puerto = config_get_string_value(t_congig, "PORT");  // Obtengo el puerto
+  puerto = config_get_string_value(config, "PORT");  // Obtengo el puerto
 
   socket_swap = crear_conexion(ip, puerto);  // Establezco conexión
 
   if (socket_swap == -1)
   {
-    log_info("swap.log", "#ERROR DE CONEXION");  // Informo error
-
-    exit(EXIT_FAILURE);  // Termino el programa
+    log_error(logger, "#ERROR DE CONEXION");  // Informo error
+    log_destroy(logger);
+    config_destroy(config);
+    return EXIT_FAILURE;  // Termino el programa
   }
   else
   {
-    log_info("swap.log",
+    log_info(logger,
              "## Conectado a Kernel Memory");  // Loggeo el comentario de
                                                // conexion iniciada
   };
-  saludar("swap");
+  //Handshake con kernel memory 
+  enviar_handshake (MID_SWAP,socket_swap);
+  int id_modulo = recibir_handshake(socket_swap);
+  if(id_modulo != MID_SWAP){
+    log_error(logger, "## Error en el Handshake con Kernel Memory");
+    close(socket_swap);
+    log_destroy(logger);
+    config_destroy(config);
+    return EXIT_FAILURE;
+  }
+  log_info(logger, "## Handshake exitoso con Kernel Memory");
   return 0;
 }
