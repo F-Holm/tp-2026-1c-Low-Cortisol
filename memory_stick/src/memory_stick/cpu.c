ba6c65a63bab52beab8ea15c3df1169a30c293ea
F-Holm <moshishafcdodgecharger@gmail.com>
2026-04-16 00:52:59 +0000

    memory-stick: move threads functions to cpu.h

--- memory_stick/src/memory_stick/cpu.c
+++ memory_stick/src/memory_stick/cpu.c
@@ -1,9 +1,7 @@
 #include "memory_stick/cpu.h"
 
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-
+#include "utils/client.h"
+#include "utils/msg.h"
 #include "utils/server.h"
 
 int create_server_cpu(void)
@@ -17,4 +15,117 @@ uint16_t get_puerto_cpu(int socket_server_cpu)
   socklen_t len = sizeof(addr);
   getsockname(socket_server_cpu, (struct sockaddr*)&addr, &len);
   return ntohs(addr.sin_port);
+}
+
+void iterator(void* value)
+{
+  shutdown(*((int*)value), SHUT_RDWR);
+}
+
+void* hilo_escucha_cpu(void* datos_hilo_escucha_void)
+{
+  int socket_espera_cpu =
+      ((t_datos_hilo_escucha*)datos_hilo_escucha_void)->socket_fd;
+  t_log* logger = ((t_datos_hilo_escucha*)datos_hilo_escucha_void)->logger;
+
+  t_list* lista_sockets = list_create();
+  pthread_mutex_t mutex_lista_sockets;
+  pthread_mutex_init(&mutex_lista_sockets, NULL);
+  atomic_bool todos_terminaron = true;
+  pthread_cond_t cond_fin_hilo_escucha;
+  pthread_cond_init(&cond_fin_hilo_escucha, NULL);
+
+  while (true)
+  {
+    int* socket_cpu = malloc(sizeof(int));
+    *socket_cpu = esperar_cliente(socket_espera_cpu);
+    if (socket_cpu <= 0)
+      break;
+
+    log_info(logger, "## Conexión exitosa con Kernel Memory");
+
+    // Handshake con CPU
+    int id_modulo = recibir_handshake(*socket_cpu);
+    if (id_modulo != MID_KERNEL_MEMORY)
+    {
+      close(*socket_cpu);
+      log_error(logger, "## Error en el Handshake con CPU");
+      continue;
+    }
+    enviar_handshake(MID_MEMORY_STICK, *socket_cpu);
+    log_info(logger, "## Handshake exitoso con Kernel Memory");
+
+    // Obtener ID
+    int codigo_operacion = recibir_operacion(*socket_cpu);
+    if (codigo_operacion != OP_IP)
+    {
+      close(*socket_cpu);
+      log_error(logger, "## Error en la recepción del ID de la CPU");
+      continue;
+    }
+    char* id_cpu = recibir_string(*socket_cpu);
+    log_info(logger, "## CPU %s Conectada", id_cpu);
+    free(id_cpu);
+
+    // Iniciar y liberar hilo
+    t_datos_hilo_cpu* datos_hilo_cpu = malloc(sizeof(t_datos_hilo_cpu));
+    datos_hilo_cpu->socket_fd = socket_cpu;
+    datos_hilo_cpu->lista_sockets = lista_sockets;
+    datos_hilo_cpu->mutex_lista_sockets = &mutex_lista_sockets;
+    datos_hilo_cpu->todos_terminaron = &todos_terminaron;
+    datos_hilo_cpu->cond_fin_hilo_escucha = &cond_fin_hilo_escucha;
+    pthread_mutex_lock(&mutex_lista_sockets);
+    if (list_is_empty(lista_sockets))
+      atomic_store(&todos_terminaron, false);
+    list_add(lista_sockets, socket_cpu);
+    pthread_mutex_unlock(&mutex_lista_sockets);
+    pthread_t hilo_cpu;
+    pthread_create(&hilo_cpu, NULL, manejar_cliente_cpu, datos_hilo_cpu);
+    pthread_detach(hilo_cpu);
+  }
+
+  log_info(logger, "## Cerrando servidor");
+  pthread_mutex_lock(&mutex_lista_sockets);
+  list_iterate(lista_sockets, (void*)iterator);
+  while (!atomic_load(&todos_terminaron))
+    pthread_cond_wait(&cond_fin_hilo_escucha, &mutex_lista_sockets);
+  pthread_mutex_unlock(&mutex_lista_sockets);
+  list_destroy(lista_sockets);
+  return NULL;
+}
+
+void* manejar_cliente_cpu(void* datos_hilo_cpu_void)
+{
+  int* socket_cpu = ((t_datos_hilo_cpu*)datos_hilo_cpu_void)->socket_fd;
+  t_list* lista_sockets =
+      ((t_datos_hilo_cpu*)datos_hilo_cpu_void)->lista_sockets;
+  pthread_mutex_t* mutex_lista_sockets =
+      ((t_datos_hilo_cpu*)datos_hilo_cpu_void)->mutex_lista_sockets;
+  atomic_bool* todos_terminaron =
+      ((t_datos_hilo_cpu*)datos_hilo_cpu_void)->todos_terminaron;
+  pthread_cond_t* cond_fin_hilo_escucha =
+      ((t_datos_hilo_cpu*)datos_hilo_cpu_void)->cond_fin_hilo_escucha;
+  free(datos_hilo_cpu_void);
+
+  while (true)
+  {
+    int operacion = recibir_operacion(*socket_cpu);
+    if (operacion == OP_CODE_ERROR)
+      break;
+    char* buffer = recibir_string(*socket_cpu);
+    free(buffer);
+  }
+
+  // Liberar conexión y eliminar socket de la lista
+  liberar_conexion(*socket_cpu);
+  pthread_mutex_lock(mutex_lista_sockets);
+  list_remove_element(lista_sockets, socket_cpu);
+  if (list_is_empty(lista_sockets))
+  {
+    atomic_store(todos_terminaron, true);
+    pthread_cond_signal(cond_fin_hilo_escucha);
+  }
+  pthread_mutex_unlock(mutex_lista_sockets);
+  free(socket_cpu);
+  return NULL;
 }
