f3fac6a6489d9fd59d03bb84a06755d6d5ca0a41
F-Holm <moshishafcdodgecharger@gmail.com>
2026-04-25 17:38:14 +0000

    memory-stick: socket_cpu ya no es un puntero & se agregó void cerrar_hilo_cpu()

--- memory_stick/src/memory_stick/cpu.h
+++ memory_stick/src/memory_stick/cpu.h
@@ -22,7 +22,7 @@ typedef struct
 
 typedef struct
 {
-  int* socket_cpu;
+  int socket_cpu;
   t_list* lista_sockets;
   pthread_mutex_t* mutex_lista_sockets;
   pthread_cond_t* cond_fin_hilo_escucha;
@@ -32,22 +32,22 @@ int create_server_cpu(t_log* logger);
 uint16_t get_puerto_cpu(int socket_server_cpu);
 void iterator_shutdown(void* value);
 t_datos_hilo_cpu* inicializar_datos_hilo_cpu(
-    int* socket_cpu, t_list* lista_sockets,
-    pthread_mutex_t* mutex_lista_sockets,
+    int socket_cpu, t_list* lista_sockets, pthread_mutex_t* mutex_lista_sockets,
     pthread_cond_t* cond_fin_hilo_escucha);
 bool crear_hilo_cpu(t_datos_hilo_cpu* datos_hilo_cpu, t_log* logger);
 void cerrar_hilo_escucha(t_list* lista_sockets,
                          pthread_mutex_t* mutex_lista_sockets,
                          pthread_cond_t* cond_fin_hilo_escucha,
-                         t_datos_hilo_escucha* params);
+                         t_datos_hilo_escucha* pardatos_hilo_escuchaams);
 bool handshake_cpu(int socket_cpu, t_log* logger);
 char* obtener_id_cpu(int socket_cpu, t_log* logger);
-bool atender_nueva_cpu(t_datos_hilo_escucha* params, int* socket_cpu,
+bool atender_nueva_cpu(t_datos_hilo_escucha* datos_hilo_escucha, int socket_cpu,
                        t_list* lista_sockets,
                        pthread_mutex_t* mutex_lista_sockets,
                        pthread_cond_t* cond_fin_hilo_escucha);
 void* hilo_escucha_cpu(void* datos_hilo_escucha_void);
 void* manejar_cliente_cpu(void* datos_hilo_cpu_void);
+void cerrar_hilo_cpu(t_datos_hilo_cpu* datos_hilo_cpu);
 bool crear_servidor_cpu(pthread_t* thread_server_cpu, int socket_servidor_cpu,
                         t_log* logger);
 
