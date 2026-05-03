071c883e2271b7ef0540ba3ff64347d124823e49
F-Holm <moshishafcdodgecharger@gmail.com>
2026-05-03 16:37:54 -0300

    cpu: mejorar manejo de errores

--- cpu/src/cpu/cpu.c
+++ cpu/src/cpu/cpu.c
@@ -8,12 +8,13 @@
 void iniciar_hilo(void* arg)
 {
   t_cpu* cpu = (t_cpu*)arg;
-  pthread_create(&cpu->hilos.kernel_memory_hilo, NULL, escuchar_kernel_memory, cpu);
+  pthread_create(&cpu->hilos.kernel_memory_hilo, NULL, escuchar_kernel_memory,
+                 cpu);
 
   log_info(cpu->logger, "Hilo de escucha de Kernel Memory iniciado");
 }
 
-void iterator(void* value)
+void iterator_close_socket(void* value)
 {
   close(*((int*)value));
   free(value);
@@ -63,7 +64,7 @@ bool iniciar_conexion_scheduler(t_cpu* cpu)
   // CONEXION CON EL KERNEL SCHEDULER
   char* ip_kernel_scheduler =
       config_get_string_value(cpu->config, "KERNEL_SCHEDULER_IP");
-  
+
   char* puerto_kernel_scheduler =
       config_get_string_value(cpu->config, "KERNEL_SCHEDULER_PUERTO");
 
@@ -95,11 +96,10 @@ bool iniciar_conexion_scheduler(t_cpu* cpu)
   return true;
 }
 
-
 bool iniciar_conexion_kmemory(t_cpu* cpu)
 {
   // CONEXION CON EL KERNEL MEMORY
-  char* ip_kernel_memory = 
+  char* ip_kernel_memory =
       config_get_string_value(cpu->config, "KERNEL_MEMORY_IP");
 
   char* puerto_kernel_memory =
@@ -132,19 +132,21 @@ bool iniciar_conexion_kmemory(t_cpu* cpu)
   return true;
 }
 
-
 bool conexion_memory_stick(t_cpu* cpu, int nuevo_socket)
 {
   // Handshake con memory stick
-  enviar_handshake(MID_CPU, nuevo_socket);
+  if (!enviar_handshake(MID_CPU, nuevo_socket))
+  {
+    log_error(cpu->logger, "## Error en el Handshake con Memory stick");
+    close(nuevo_socket);
+    return false;
+  }
 
   int id_modulo = recibir_handshake(nuevo_socket);
   if (id_modulo != MID_MEMORY_STICK)
   {
     log_error(cpu->logger, "## Error en el Handshake con Memory stick");
     close(nuevo_socket);
-    log_destroy(cpu->logger);
-    config_destroy(cpu->config);
     return false;
   }
   log_info(cpu->logger, "## Handshake exitoso con Memory stick");
@@ -152,8 +154,8 @@ bool conexion_memory_stick(t_cpu* cpu, int nuevo_socket)
   return true;
 }
 
-
-bool manejar_paquete(t_cpu* cpu, t_list* lista_paquete, char ip_stick[16], char puerto_stick[6])
+bool manejar_paquete(t_cpu* cpu, t_list* lista_paquete, char ip_stick[16],
+                     char puerto_stick[6])
 {
   if (list_size(lista_paquete) != 2)
   {
@@ -173,7 +175,6 @@ bool manejar_paquete(t_cpu* cpu, t_list* lista_paquete, char ip_stick[16], char 
   return true;
 }
 
-
 void* escuchar_kernel_memory(void* arg)
 {
   t_cpu* cpu = (t_cpu*)arg;
@@ -185,32 +186,44 @@ void* escuchar_kernel_memory(void* arg)
     char puerto_stick[6];
     int nuevo_socket;
     int codigo_operacion = recibir_operacion(cpu->socket_kernel_memory);
-    if (codigo_operacion != OP_PAQUETE)
+    if (codigo_operacion == OP_CODE_ERROR)
     {
-      log_error(cpu->logger, "## Error en el tipo de operación");
-      log_destroy(cpu->logger);
-      config_destroy(cpu->config);
-      return NULL;
+      log_error(cpu->logger, "## Cerrando hilo");
+      break;
     }
-    
+    else if (codigo_operacion != OP_PAQUETE)
+    {
+      log_error(cpu->logger, "## Tipo de operación no válido");
+      continue;
+    }
+
     lista_paquete = recibir_paquete(cpu->socket_kernel_memory);
-    if(!manejar_paquete(cpu, lista_paquete, ip_stick, puerto_stick))
-      return NULL;
+    if (!manejar_paquete(cpu, lista_paquete, ip_stick, puerto_stick))
+      continue;
 
     nuevo_socket = crear_conexion(ip_stick, puerto_stick);
 
     if (nuevo_socket <= 0)
     {
       log_error(cpu->logger, "## Error en la conexión con Memory stick");
-      return false;
+      continue;
     }
 
     log_info(cpu->logger,
-              "## Conectandose a memory stick con ip %s y puerto %s", ip_stick, puerto_stick);
+             "## Conectandose a memory stick con ip %s y puerto %s", ip_stick,
+             puerto_stick);
 
-    conexion_memory_stick(cpu, nuevo_socket);
+    if (!conexion_memory_stick(cpu, nuevo_socket))
+    {
+      log_error(cpu->logger, "## Error en el handshake con Memory stick");
+      continue;
+    }
 
-    enviar_string(OP_ID_CPU, cpu->id, nuevo_socket);
+    if (!enviar_string(OP_ID_CPU, cpu->id, nuevo_socket))
+    {
+      log_error(cpu->logger, "## Error en el envío de ID con Memory stick");
+      continue;
+    }
 
     int* p_socket = malloc(sizeof(int));
     *p_socket = nuevo_socket;
@@ -218,7 +231,7 @@ void* escuchar_kernel_memory(void* arg)
     list_add(cpu->memory_sticks, p_socket);
   }
 
-  list_destroy_and_destroy_elements(cpu->memory_sticks, (void*)iterator);
+  list_destroy_and_destroy_elements(cpu->memory_sticks, (void*)iterator_close_socket);
   return NULL;
 }
 
@@ -228,11 +241,11 @@ void cerrar_modulo(t_cpu* cpu)
     close(cpu->socket_kernel_memory);
 
   if (cpu->socket_kernel_scheduler > 0)
-    close(cpu-> socket_kernel_scheduler);
+    close(cpu->socket_kernel_scheduler);
 
   if (cpu->logger != NULL)
     log_destroy(cpu->logger);
-  
+
   if (cpu->config != NULL)
     config_destroy(cpu->config);
 }
