46344732b963c216da2e244b5100bf9929358a7d
FSoteloLabat <fsotelolabat@frba.utn.edu.ar>
2026-05-19 14:18:57 +0000

    memory.c fix

--- kernel_scheduler/src/kernel_scheduler/memory.h
+++ kernel_scheduler/src/kernel_scheduler/memory.h
@@ -2,6 +2,7 @@
 #define KERNEL_SCHEDULER_MEMORY_H_
 
 #include <commons/log.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -10,8 +11,8 @@
 #include "utils/kernel_scheduler_cpu.h"
 
 bool allocate_memory(t_syscall_memory* mem_alloc, t_logger* logger,
-                     t_socket_kernel_memory* socket_km);
+                     t_socket_kernel_memory* socket_km, int socket_server);
 bool free_memory(t_syscall_memory* mem_free, t_logger* logger,
-                 t_socket_kernel_memory* socket_km);
+                 t_socket_kernel_memory* socket_km, int socket_server);
 
 #endif /* KERNEL_SCHEDULER_MEMORY_H_ */
