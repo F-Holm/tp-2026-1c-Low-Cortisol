869aae593d5042fdde83df47e4716490f6f24e85
F-Holm <moshishafcdodgecharger@gmail.com>
2026-05-31 23:47:51 -0300

    kernel-scheduler: suspendido.h creado, todavía no está terminado

---
+++ kernel_scheduler/src/kernel_scheduler/suspendido.c
@@ -0,0 +1,104 @@
+#include "kernel_scheduler/suspensor.h"
+
+static t_datos_hilo_suspensor* inicializar_datos_hilo_suspendido(t_colas* colas)
+{
+  t_datos_hilo_suspendido* datos = malloc(t_datos_hilo_suspendido);
+  pthread_mutex_init(&(datos->mutex_estado), NULL);
+  datos->estado = EH_ESPERANDO_PROCESO;
+  datos->mutex_suspender_des_suspender = colas.
+
+                                         return datos_hilo_suspensor;
+}
+
+static t_datos_hilo_suspensor* inicializar_datos_hilo_suspensor(
+    t_colas* colas, int suspension_timeout)
+{
+  t_datos_hilo_suspensor* datos = malloc(t_datos_hilo_suspensor);
+  datos->suspension_timeout = suspension_timeout;
+  datos->datos = inicializar_datos_hilo_suspendido(colas);
+  return datos;
+}
+
+static t_datos_hilo_des_suspensor* inicializar_datos_hilo_des_suspensor(
+    t_colas* colas)
+{
+  t_datos_hilo_des_suspensor* datos = malloc(t_datos_hilo_des_suspensor);
+  datos->datos = inicializar_datos_hilo_suspendido(colas);
+  return datos;
+}
+
+static void terminar_hilo_suspensor(
+    t_datos_hilo_suspensor* datos_hilo_suspensor);
+
+static void terminar_hilo_des_suspensor(
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+static void esperar_hilo_suspensor(
+    t_datos_hilo_suspensor* datos_hilo_suspensor);
+
+static void esperar_hilo_des_suspensor(
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+static void destruir_hilo_suspensor(
+    t_datos_hilo_suspensor* datos_hilo_suspensor);
+
+static void destruir_hilo_des_suspensor(
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+static void bloquear_hilo_suspensor(
+    t_datos_hilo_suspensor* datos_hilo_suspensor);
+
+static void bloquear_hilo_des_suspensor(
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+static void desbloquear_hilo_suspensor(
+    t_datos_hilo_suspensor* datos_hilo_suspensor);
+
+static void desbloquear_hilo_des_suspensor(
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+void iniciar_hilos_suspendido(
+    t_colas* colas, int suspension_timeout,
+    t_datos_hilo_suspensor** datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor** datos_hilo_des_suspensor)
+{
+  *datos_hilo_suspensor =
+      inicializar_datos_hilo_suspensor(colas, suspension_timeout);
+  *datos_hilo_des_suspensor = inicializar_datos_hilo_des_suspensor(colas);
+  iniciar_hilo_suspensor(*datos_hilo_suspensor);
+  iniciar_hilo_des_suspensor(*datos_hilo_des_suspensor);
+}
+
+void terminar_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor)
+{
+  terminar_hilo_suspensor(datos_hilo_suspensor);
+  terminar_hilo_des_suspensor(datos_hilo_des_suspensor);
+  esperar_hilo_suspensor(datos_hilo_suspensor);
+  esperar_hilo_des_suspensor(datos_hilo_des_suspensor);
+}
+
+void destruir_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor)
+{
+  destruir_hilo_suspensor(datos_hilo_suspensor);
+  destruir_hilo_des_suspensor(datos_hilo_des_suspensor);
+}
+
+void bloquear_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor)
+{
+  bloquear_hilo_suspensor(datos_hilo_suspensor);
+  bloquear_hilo_des_suspensor(datos_hilo_des_suspensor);
+}
+
+void desbloquear_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor)
+{
+  desbloquear_hilo_suspensor(datos_hilo_suspensor);
+  desbloquear_hilo_des_suspensor(datos_hilo_des_suspensor);
+}
---
+++ kernel_scheduler/src/kernel_scheduler/suspendido.h
@@ -0,0 +1,65 @@
+#ifndef KERNEL_SCHEDULER_SUSPENDIDO_H_
+#define KERNEL_SCHEDULER_SUSPENDIDO_H_
+
+#include <pthread.h>
+
+#include "kernel_scheduler/queue.h"
+#include "utils/logger.h"
+
+typedef enum
+{
+  EH_EJECUTANDO,
+  EH_ESPERANDO_PROCESO,
+  EH_BLOQUEADO,
+  EH_FINALIZANDO,
+  EH_FINALIZADO
+} t_estado_hilo;
+
+typedef struct
+{
+  pthread_t hilo;
+  pthread_mutex_t mutex_estado;
+  int estado;
+  pthread_mutex_t* mutex_suspender_des_suspender;
+  pthread_cond_t* esperar_proceso;
+  pthread_cond_t desbloquear;
+  t_cola_ready* ready;
+  t_lista* block;
+  t_lista* susp_block;
+  t_lista* susp_ready;
+  t_logger* logger;
+} t_datos_hilo_suspendido;
+
+typedef struct
+{
+  int suspension_timeout;
+  t_datos_hilo_suspendido* datos;
+} t_datos_hilo_suspensor;
+
+typedef struct
+{
+  t_datos_hilo_suspendido* datos;
+} t_datos_hilo_des_suspensor;
+
+void iniciar_hilos_suspendido(
+    t_colas* colas, int suspension_timeout,
+    t_datos_hilo_suspensor** datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor** datos_hilo_des_suspensor);
+
+void terminar_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+void destruir_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+void bloquear_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+void desbloquear_hilos_suspendido(
+    t_datos_hilo_suspensor* datos_hilo_suspensor,
+    t_datos_hilo_des_suspensor* datos_hilo_des_suspensor);
+
+#endif /* KERNEL_SCHEDULER_SUSPENDIDO_H_ */
