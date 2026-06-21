8d83d530ef53eef40bfe5bd5d337cb43f9274033
LucasPanzera <lucaspanzera614@gmail.com>
2026-06-21 02:50:59 +0000

    CPU: envio string

--- cpu/src/cpu/memoria.c
+++ cpu/src/cpu/memoria.c
@@ -48,7 +48,7 @@ t_segmento* buscar_segmento_por_id(t_list* tablaSegmentos,
 
 void seg_fault_KS(t_cpu* cpu, uint32_t pid)
 {
-  if (!enviar_buffer(OP_SEG_FAULT, &pid, sizeof(uint32_t),
+  if (!enviar_string(OP_SEG_FAULT,"SEGMENTATION FAULT",
                      cpu->socket_kernel_scheduler))
   {
     log_error(cpu->logger, "## Fallo en el envío de segmentation fault");
