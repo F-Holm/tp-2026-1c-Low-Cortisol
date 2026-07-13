dc9aad6693ebcbc042b32f10cdefd2905d151880
F-Holm <moshishafcdodgecharger@gmail.com>
2026-07-13 11:58:53 -0300

    kernel-scheduler: ahora se espera respuesta luego de una peticion de suspensión

--- utils/src/utils/msg.h
+++ utils/src/utils/msg.h
@@ -64,6 +64,8 @@ typedef enum
   OP_TAMANIO_PROCESO,           // tamaño del proceso en memoria
   OP_NUEVO_MEMORY_STICK,        // avisar que se conectó un memory stick
   OP_SUSPENDER_PROCESO,         // no devuelve nada porque no puede fallar
+  OP_SUSPENSION_EXITOSA,
+  OP_SUSPENSION_NO_EXITOSA,
   OP_DES_SUSPENDER_PROCESO,     // devuelve uno de los siguientes 2 mensajes
   OP_DES_SUSPENSION_EXITOSA,
   OP_DES_SUSPENSION_NO_EXITOSA,
