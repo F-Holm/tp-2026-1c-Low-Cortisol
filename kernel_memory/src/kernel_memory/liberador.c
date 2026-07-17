851545f01abe3a74b2a95cee3cd780d11b772a72
incutti <mateincu@gmail.com>
2026-07-17 14:42:25 +0000

    race condition al cierre de cpus desde km solucionada

--- kernel_memory/src/kernel_memory/liberador.c
+++ kernel_memory/src/kernel_memory/liberador.c
@@ -11,7 +11,6 @@ void cerrar_cpu(t_datos_cpu* cpu)
   if (cpu->socket_cpu != -1)
   {
     terminar_comunicacion(cpu->socket_cpu);
-    cpu->socket_cpu = -1;
   }
 }
 
@@ -72,11 +71,19 @@ void liberar_datos_kernel_mem(t_datos_kernel_mem* datos_kernel)
   }
   if (datos_kernel->cpus_conectados != NULL)
   {
-    for (int i = 0; i < list_size(datos_kernel->cpus_conectados); i++)
+    /*for (int i = 0; i < list_size(datos_kernel->cpus_conectados); i++)
     {
       t_datos_cpu* cpu =
           (t_datos_cpu*)list_get(datos_kernel->cpus_conectados, i);
-      liberar_datos_cpu(cpu);
+      //liberar_datos_cpu(cpu);
+      list_remove(datos_kernel->cpus_conectados, cpu);
+      shutdown(cpu->socket_cpu, SHUT_RDWR);
+    }*/
+    while(!list_is_empty(datos_kernel->cpus_conectados))
+    {
+      t_datos_cpu* cpu = list_remove(datos_kernel->cpus_conectados, 0);
+      shutdown(cpu->socket_cpu, SHUT_RDWR);
+      free(cpu);
     }
     list_destroy(datos_kernel->cpus_conectados);
   }
