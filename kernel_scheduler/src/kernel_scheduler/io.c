66fb818fdf64942766f62d803317f1e8d260852f
FSoteloLabat <fsotelolabat@frba.utn.edu.ar>
2026-07-31 23:10:26 +0000

    ks: fix cierre hilos io

--- kernel_scheduler/src/kernel_scheduler/io.c
+++ kernel_scheduler/src/kernel_scheduler/io.c
@@ -129,8 +129,11 @@ void cerrar_io(t_io* io)
   {
     if (io[i].socket_io != -1)
     {
+
       pthread_mutex_lock(&(io[i].mutex_fin));
-      io[i].cerrar_hilo = true;
+      if(!io[i].cerrar_hilo){
+         io[i].cerrar_hilo = true;
+      }
       pthread_mutex_unlock(&(io[i].mutex_fin));
       shutdown(io[i].socket_io, SHUT_RDWR);
       pthread_mutex_lock(&(io[i].lista_io->mutex_lista_io));
@@ -321,6 +324,13 @@ static void cerrar_hilo_io(t_io* io)
   pthread_mutex_unlock(&(io->lista_io->mutex_lista_io));
   pthread_mutex_destroy(&(io->lista_io->mutex_lista_io));
   list_destroy(io->lista_io->lista_io);
+  bool chequear_io = chequeo_cerrar_hilo(io);
+  if (!chequear_io)
+  {
+    pthread_mutex_lock(&(io->mutex_fin));
+    io->cerrar_hilo = true;
+    pthread_mutex_unlock(&(io->mutex_fin));
+  }
   free(io->lista_io);
 }
 
