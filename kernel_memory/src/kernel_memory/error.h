5a9975c0361af82d340847f45a3428394c6b570a
F-Holm <moshishafcdodgecharger@gmail.com>
2026-09-15 22:06:42 -0300

    docs(kernel_memory): document error.h

--- kernel_memory/src/kernel_memory/error.h
+++ kernel_memory/src/kernel_memory/error.h
@@ -4,6 +4,11 @@
 #include "kernel_memory/configurator.h"
 #include "kernel_memory/structs.h"
 
+/** @brief Logs a handshake failure naming @p error_section and closes the
+ * socket. */
 void send_handshake_error(t_log* logger, int client_socket,
                           char* error_section);
+
+/** @brief Logs an init failure naming @p error_section and closes the socket.
+ */
 void send_init_error(t_log* logger, int client_socket, char* error_section);
