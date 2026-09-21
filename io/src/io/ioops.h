dfd6346c98fe9b2385aaa8eed44487d69e5358f4
F-Holm <moshishafcdodgecharger@gmail.com>
2026-09-21 17:10:12 -0300

    refactor(io): drop the unused netdb and sys/socket includes from ioops.h

--- io/src/io/ioops.h
+++ io/src/io/ioops.h
@@ -1,14 +1,11 @@
 #pragma once
 
 #include <assert.h>
-#include <netdb.h>
-#include <signal.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <unistd.h>
 
 #include "utils.h"
