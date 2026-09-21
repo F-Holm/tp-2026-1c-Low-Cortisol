a939bb50b802fb55374638771d58422be9013771
F-Holm <moshishafcdodgecharger@gmail.com>
2026-09-21 19:21:51 -0300

    refactor(kernel_scheduler): receive memory query replies through receive_km_opcode

--- kernel_scheduler/src/kernel_scheduler/scheduler/memory_query.c
+++ kernel_scheduler/src/kernel_scheduler/scheduler/memory_query.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "kernel_scheduler/scheduler/kernel_memory_reply.h"
 #include "kernel_scheduler/scheduler/queues.h"
 #include "kernel_scheduler/shutdown.h"
 #include "utils/log.h"
@@ -63,87 +64,40 @@ int process_size_no_logger(t_queues* queues, uint32_t pid)
 
 static int receive_space(t_queues* queues)
 {
-  int op_code = receive_op_code(queues->km_socket);
+  if (RECEIVE_KM_OPCODE(queues, OP_FREE_MEMORY) == OP_CODE_ERROR)
+    return -1;
 
-  switch (op_code)
-  {
-    case OP_FREE_MEMORY:
-      int space;
-      int* aux = receive_buffer(&space, queues->km_socket);
-      space = *aux;
-      free(aux);
-      log_trace(queues->logger, "Space available: %d", space);
-      return space;
-    case OP_NEW_MEMORY_STICK:
-      free(receive_string(queues->km_socket));
-      create_resumption_routine_thread(queues);
-      return receive_space(queues);
-      break;
-    case OP_MEMORY_CORRUPTED:
-      close_kernel_scheduler(SR_CORRUPTED_MEMORY);
-      break;
-    default:
-      close_kernel_scheduler(SR_KERNEL_MEMORY_CONNECTION_FAILURE);
-      break;
-  }
-  return -1;
+  int space;
+  int* aux = receive_buffer(&space, queues->km_socket);
+  space = *aux;
+  free(aux);
+  log_trace(queues->logger, "Space available: %d", space);
+  return space;
 }
 
 static int receive_size(t_queues* queues)
 {
-  int op_code = receive_op_code(queues->km_socket);
+  if (RECEIVE_KM_OPCODE(queues, OP_PROCESS_SIZE) == OP_CODE_ERROR)
+    return -1;
 
-  switch (op_code)
-  {
-    case OP_PROCESS_SIZE:
-      int space;
-      int* aux = receive_buffer(&space, queues->km_socket);
-      space = *aux;
-      free(aux);
-      log_trace(queues->logger, "Process size: %d", space);
-      return space;
-      break;
-    case OP_NEW_MEMORY_STICK:
-      free(receive_string(queues->km_socket));
-      create_resumption_routine_thread(queues);
-      return receive_size(queues);
-      break;
-    case OP_MEMORY_CORRUPTED:
-      close_kernel_scheduler(SR_CORRUPTED_MEMORY);
-      break;
-    default:
-      close_kernel_scheduler(SR_KERNEL_MEMORY_CONNECTION_FAILURE);
-      break;
-  }
-  return -1;
+  int space;
+  int* aux = receive_buffer(&space, queues->km_socket);
+  space = *aux;
+  free(aux);
+  log_trace(queues->logger, "Process size: %d", space);
+  return space;
 }
 
 static int receive_size_no_logger(t_queues* queues)
 {
-  int op_code = receive_op_code(queues->km_socket);
+  if (RECEIVE_KM_OPCODE(queues, OP_PROCESS_SIZE) == OP_CODE_ERROR)
+    return -1;
 
-  switch (op_code)
-  {
-    case OP_PROCESS_SIZE:
-      int space;
-      int* aux = receive_buffer(&space, queues->km_socket);
-      space = *aux;
-      free(aux);
-      return space;
-      break;
-    case OP_NEW_MEMORY_STICK:
-      free(receive_string(queues->km_socket));
-      create_resumption_routine_thread(queues);
-      return receive_size_no_logger(queues);
-      break;
-    case OP_MEMORY_CORRUPTED:
-      close_kernel_scheduler(SR_CORRUPTED_MEMORY);
-      break;
-    default:
-      close_kernel_scheduler(SR_KERNEL_MEMORY_CONNECTION_FAILURE);
-      break;
-  }
-  return -1;
+  int space;
+  int* aux = receive_buffer(&space, queues->km_socket);
+  space = *aux;
+  free(aux);
+  return space;
 }
 
 static int process_size_no_mutex_no_logger(t_queues* queues, uint32_t pid)
